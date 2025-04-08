a27f8e7eb53652bb30b7a54e60ca1daec3d52aeb
imanali2237 <imanali341012@gmail.com>
2025-04-09 00:05:43 +0500

    pushing the code of Hello world

---
+++ com/mylearning/basics/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.mylearning.basics;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Finally Started Java , now just be consistent now plz");
+    }
+}
